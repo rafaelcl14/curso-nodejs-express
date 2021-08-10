7f5ab719cf8ec40e61bf6003efef0f86e550d9b7
rafaelcl14 <rafaelcl1497@gmail.com>
2021-08-10 16:50:08 -0400

    add files

---
+++ controllers/atendimentos.js
@@ -0,0 +1,31 @@
+const Atendimento = require("../models/atendimentos");
+
+module.exports = (app) => {
+  app.get("/atendimentos", (req, res) => {
+    Atendimento.lista(res);
+  });
+
+  app.get("/atendimentos/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    Atendimento.buscarPorId(id, res);
+  });
+
+  app.post("/atendimentos", (req, res) => {
+    const atendimento = req.body;
+
+    Atendimento.adiciona(atendimento, res);
+  });
+
+  app.patch("/atendimentos/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const valores = req.body;
+
+    Atendimento.altrera(id, valores, res);
+  });
+
+  app.delete("/atendimentos/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    Atendimento.deleta(id, res);
+  });
+};
